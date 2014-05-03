8caa967a5e8609f528c99ca73d4c99448348c445
Adam K Chew <adamkchew@gmail.com>
2014-05-03 15:26:43 -0400

    Added basic maps
    Added basic maps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,13 +15,3 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
-
-  var map;
-  require(["esri/map", "dojo/domReady!"], function(Map) { 
-    map = new Map("mapDiv", {
-      center: [-56.049, 38.485],
-      zoom: 3,
-      basemap: "streets"
-    });
-  });
-     
