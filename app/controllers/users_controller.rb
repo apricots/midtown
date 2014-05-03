d60f52d152519f80538da87ffa90c390fb160cc5
apricots <potatonisha@gmail.com>
2014-05-03 15:02:03 -0400

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def index
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save 
+      flash[:notice] = "Hi #{@user.username}! You have created a new account!"
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :address)
+  end
 end
