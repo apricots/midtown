e8d9b213873530b064f268f78dae7205c3c71fa5
apricots <potatonisha@gmail.com>
2014-05-04 03:16:12 -0400

    stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :address)
+    params.require(:user).permit(:username, :address, :longitude, :latitude)
   end
 end
